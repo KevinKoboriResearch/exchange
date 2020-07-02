f65ef323731e8ed78d72ce25a2bedd073d226fa4
kevinkobori <kevin.kobori@hotmail.com>
2020-07-01 23:10:54 -0300

    semifinal moedas

--- src/.nuxt/App.js
+++ src/.nuxt/App.js
@@ -4,8 +4,6 @@ import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch,
 import NuxtError from '../layouts/error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
 
-import '../node_modules/vuetify/dist/vuetify.css'
-
 import _6f6c098b from '../layouts/default.vue'
 
 const layouts = { "_default": sanitizeComponent(_6f6c098b) }
@@ -106,6 +104,10 @@ export default {
     isFetching () {
       return this.nbFetching > 0
     },
+
+    isPreview () {
+      return Boolean(this.$options.previewData)
+    },
   },
 
   methods: {
@@ -181,10 +183,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
--- src/components/exchange/Coins.vue
+++ src/components/exchange/Coins.vue
@@ -1,180 +1,150 @@
 <template>
-<div>
-  <!-- <p> {{ apiData | json}} </p> -->
-  <v-card class="pa-2">
-          <v-row class="pa-2" style="flex-wrap: nowrap;">
-            <v-col 
-              class="flex-grow-1 flex-shrink-0 pa-1"
-            >
-              <v-text-field label="Moeda" clearable solo-inverted disabled></v-text-field>
-            </v-col>
-            <v-col 
-              class="flex-grow-1 flex-shrink-0 pa-1"
-            >
-              <v-text-field label="Real" v-model="labelReal" solo-inverted clearable></v-text-field>
-            </v-col>
-            <v-col
-              class="flex-grow-1 flex-shrink-0 pa-1"
-            >
-                <v-text-field label="Valor da Moeda" v-model="labelMoeda" solo-inverted clearable></v-text-field>
-            </v-col>
-          </v-row>
-    <!-- <v-list> -->
-      <v-list
-        v-for="coin in apiData" :key="coin"
-        v-model="coin.active"
-        no-action
-      >
-        <!-- <template v-slot:activator> -->
-
-            <v-card
-                class="mx-auto"
-                color="deep-purple darken-1"
-                tile
-              >
-          <v-row class="pa-3" style="flex-wrap: nowrap;">
-          <v-col class="flex-grow-1 flex-shrink-0 pa-0">
-            <v-card
-                class="mx-auto"
-                color="#64B5F"
-                tile
-                flat
-              >
-            <v-text>&nbsp;{{ coin.name }}</v-text>
+  <div>
+    <!-- <p> {{ apiData | json}} </p> -->
+    <v-card class="pa-2">
+      <v-row class="pa-2" style="flex-wrap: nowrap;">
+        <v-col class="flex-grow-1 flex-shrink-0 pa-1">
+          <v-hover v-slot:default="{ hover }">
+            <v-card :elevation="hover ? 4 : 0">
+              <div @click="cotation = !cotation" >
+                <v-text-field persistent-hint :hint="cotation ? 'high' : 'low'" :label="cotation ? 'low' : 'high'" clearable solo-inverted 
+                disabled></v-text-field>
+              </div>
             </v-card>
-            <!-- </v-card> -->
-            <!-- <v-text> {{ coin.name }} </v-text> -->
-          </v-col>
-          <!-- <v-spacer/> -->
-          <v-col class="flex-grow-1 flex-shrink-0 pa-0">
-          <!-- <v-list-item-content class="flex-grow-1 flex-shrink-0 pa-1"> -->
-            <!-- <div v-if="real"> -->
-              <!-- <v-text-field :label="(coin.code) + ' ' + (real / coin.low)"> {{ coin.code }} {{real / coin.low | currencydecimal}}</v-text-field> -->
-            <!-- </div> -->
-            <!-- <div v-else> -->
-              <!-- <v-card
-                class="mx-auto"
-                color="#26a6da"
-              > -->
-              <v-card
-                class="mx-auto"
-                color="#42A5F"
-                tile
-                flat
-              >
-               <v-text>&nbsp;&nbsp;{{coin.code}} {{real / coin.low | currencydecimal}}</v-text>
-              </v-card>
-               <!-- <v-text-field :label="(coin.code) + ' ' + (real / coin.low)" solo-inverted disabled></v-text-field> -->
-              <!-- </v-card> -->
-            <!-- </div> -->
-            <!-- <v-text-field :label="'R$ ' + (moeda * coin.low)" v-model="real" clearable solo-inverted disabled></v-text-field> -->
-          <!-- </v-list-item-content> -->
-          </v-col>
-          <!-- <v-spacer/> -->
-          <!-- <v-list-item-content class="flex-grow-1 flex-shrink-0 pa-1"> -->
-            <v-col class="flex-grow-1 flex-shrink-0 pa-0">
-            <!-- <div v-if="moeda"> -->
-              <!-- <v-text-area>  {{ coin.code }} {{real / coin.low | currencydecimal}}</v-text-area> -->
-              <!-- <v-list-item-title> R$ {{ moeda * coin.low | currencydecimal}}</v-list-item-title> -->
-            <!-- </div> -->
-            <!-- <div v-else> -->
-              <v-card
-              color="#1E88E"
-              tile
-              flat
-              >
-               <v-text>&nbsp;&nbsp;R$ {{moeda * coin.low | currencydecimal}}</v-text>
-              </v-card>
-            <!-- </div> -->
-            <!-- <v-text-field :label="(coin.code) + ' ' + (real / coin.low)" v-model="moeda" clearable solo-inverted disabled>R$ +  </v-text-field> -->
-          <!-- </v-list-item-content> -->
-            </v-col>
-          </v-row>
+          </v-hover>
+        </v-col>
+        <v-col class="flex-grow-1 flex-shrink-0 pa-1">
+          <v-hover v-slot:default="{ hover }">
+            <v-card :elevation="hover ? 4 : 0">
+              <v-text-field persistent-hint hint="Real" label="R$" v-model="labelReal" solo-inverted clearable></v-text-field>
             </v-card>
-        <!-- </template> -->
-        <!-- <v-list>
-          <v-row class="pa-2" style="flex-wrap: nowrap;">
-            <v-col 
-              class="flex-grow-0.5 flex-shrink-0 pa-1"
-            >
-              <v-list-item-content>
-                  <v-text-field :value="coin.high" solo-inverted disabled></v-text-field>
-              </v-list-item-content>
-            </v-col>
-            <v-col
-              class="flex-grow-1 flex-shrink-0 pa-1"
-            >
-              <v-list-item-content>
-                <v-text-field :label="(coin.code) + ' ' + (real / coin.low)" solo-inverted disabled>R$ +  </v-text-field>
-              </v-list-item-content>
-            </v-col>
-          </v-row>
-        </v-list> -->
+          </v-hover>
+        </v-col>
+        <v-col class="flex-grow-1 flex-shrink-0 pa-1" >
+        <v-hover v-slot:default="{ hover }">
+          <v-card :elevation="hover ? 4 : 0">
+            <v-text-field persistent-hint hint="Valor da Moeda" label="Moeda" v-model="labelMoeda" solo-inverted clearable></v-text-field>
+          </v-card>
+        </v-hover>
+        </v-col>
+      </v-row>
+      <v-list v-for="coin in apiData" :key="coin" v-model="coin.active" no-action>
+        <v-row class="pa-3" style="flex-wrap: nowrap;">
+          <v-col class="flex-grow-1 flex-shrink-0 pa-1">
+            <v-card tile flat>
+              <v-hover v-slot:default="{ hover }">
+                <div v-if="cotation == true">
+                  <v-card :elevation="hover ? 4 : 1" tile flat @click="cotation = !cotation">
+                    <v-text-field persistent-hint :hint="cotation ? 'low' : 'high'" :label="(coin.name)" tile loading dense solo disabled color="grey darken-3"></v-text-field>
+                  </v-card>
+                </div>
+                <div v-else>
+                  <v-card :elevation="hover ? 4 : 1" tile flat @click="cotation = !cotation">
+                    <v-text-field persistent-hint :hint="cotation ? 'low' : 'high'" :label="(coin.name)" tile loading dense solo disabled color="grey darken-3"></v-text-field>
+                  </v-card>
+                </div>
+              </v-hover>
+            </v-card>
+          </v-col>
+          <v-col class="flex-grow-1 flex-shrink-0 pa-1">
+            <div v-if="cotation == true">
+              <v-card tile flat color="#7bdf4f">
+                <v-text-field :persistent-hint="real!=null" :hint="'R$ '+ real" :label="(coin.code) + ' ' + (div(real, coin.low))" tile loading dense solo disabled color="#7bdf4f" class="black--text"></v-text-field>
+              </v-card>
+            </div>
+            <div v-else>
+              <v-card tile flat color="#7bdf4f">
+                <v-text-field :persistent-hint="real!=null" :hint="'R$ '+ real" :label="(coin.code) + ' ' + (div(real, coin.high))" tile loading dense solo disabled color="#7bdf4f" class="black--text"></v-text-field>
+              </v-card>
+            </div>
+          </v-col>
+          <v-col class="flex-grow-1 flex-shrink-0 pa-1" dense>
+            <div v-if="cotation == true">
+              <v-card tile flat color="#7bdf4f">
+                <v-text-field :persistent-hint="moeda!=null" :hint="coin.code + ' ' + moeda" :label="'R$ ' + (mult(moeda, coin.low))" 
+                tile dense loading solo disabled color="#7bdf4f" class="black--text"></v-text-field>
+              </v-card>
+            </div>
+            <div v-else>
+              <v-card tile flat color="#7bdf4f">
+                <v-text-field :persistent-hint="moeda!=null" :hint="coin.code + ' ' + moeda" :label="'R$ ' + (mult(moeda, coin.high))" 
+                tile dense loading solo disabled color="#7bdf4f" class="black--text"></v-text-field>
+              </v-card>
+            </div>
+          </v-col>
+        </v-row>
       </v-list>
-      <!-- </v-list-group> -->
-    <!-- </v-list> -->
-  </v-card>
+    </v-card>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-  export default {
-    data() {
-      return {
-        apiData: null,
-        moeda: null,
-        moedas: null,
-        real: null,
-      }
-    },
-    async mounted () {
-      await axios.get('https://economia.awesomeapi.com.br/json/all').then(response => (this.apiData = response.data))
-    },
-    computed: {
-      labelReal: {
-        get() {
-          if(this.real == null)
-            return
-          else
-            var x = "R$ " + this.real
-            return x
-        },
-        set(val) {
-          if(val != null)
-            this.real = val.match(/\d+/)[0]
-          else
-            this.real = null
-        }
-      },
-      labelMoeda: {
-        get() {
-          if(this.moeda == null)
-            return
-          else
-            var x = "Moeda " + this.moeda
-            return x
-        },
-        set(val) {
-          if(val != null)
-            this.moeda = val.match(/\d+/)[0]
-          else
-            this.moeda = null
-        }
-      }
-    },
-    filters: {
-      currencydecimal(value) {
-        value = value.toFixed(2)
-        value = value.toString().replace(".", ",")
-        return value
-      },
+export default {
+  data() {
+    return {
+      apiData: null,
+      moeda: null,
+      cotation: false,
+      real: null,
     }
+  },
+  async mounted () {
+    await axios.get('https://economia.awesomeapi.com.br/json/all').then(response => (this.apiData = response.data))
+  },
+  methods: {
+    div(val, val2) {
+      return this.decimal(val / val2)
+    },
+    mult(val, val2) {
+      return this.decimal(val * val2)
+    },
+    decimal(value) {
+      value = value.toFixed(2)
+      value = value.toString().replace(".", ",")
+      return value
+    }
+  },
+  computed: {
+    labelReal: {
+      get() {
+        if(this.real == null)
+          return
+        else
+          var x = "R$ " + this.real
+          return x
+      },
+      set(val) {
+        var regex = /\d/g;
+        if(val != null && regex.test(val) != false)
+          this.real = val.match(/\d+/)[0]
+        else
+          this.real = null
+      }
+    },
+    labelMoeda: {
+      get() {
+        if(this.moeda == null)
+          return
+        else
+          var x = "Moeda " + this.moeda
+          return x
+      },
+      set(val) {
+        var regex = /\d/g;
+        if(val != null && regex.test(val) != false)
+          this.moeda = val.match(/\d+/)[0]
+        else
+          this.moeda = null
+      }
+    }
+  },
+  filters: {
+    currencydecimal(value) {
+      value = value.toFixed(2)
+      value = value.toString().replace(".", ",")
+      return value
+    },
   }
-</script>
-
-<style scoped>
-.leftspace{
-  margin-left: 300px;
 }
-</style>
+</script>
